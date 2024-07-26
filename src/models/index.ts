7349ca0b400d3708b68e10b48a57126479fcfc0b
Maryna Karpova <mari.artarea@gmail.com>
2024-07-26 12:34:48 +0200

    back

---
+++ src/models/index.ts
@@ -0,0 +1,32 @@
+import { AccessoryMap } from './accessory';
+import { PhoneMap } from './phone';
+import { ProductMap } from './product';
+import { Sequelize } from 'sequelize-typescript';
+import { TabletMap } from './tablet';
+import { UserMap } from './user';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in environment variables');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
+
+ProductMap(sequelize);
+PhoneMap(sequelize);
+AccessoryMap(sequelize);
+TabletMap(sequelize);
+UserMap(sequelize);
+
+export { sequelize };
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import express, { Application } from 'express';
+
+import accessoryRouter from './routes/accessories.route';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import phoneRouter from './routes/phones.route';
+import productRouter from './routes/products.router';
+import { sequelize } from './models';
+import tabletRouter from './routes/tablets.route';
+import userRouter from './routes/users.router';
+import { orderMap } from './models/order';
+import orderRouter from './routes/order.router';
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+orderMap(sequelize);
+
+sequelize
+  .sync()
+  .then(() => {
+    app.use('/products', productRouter);
+    app.use('/tablets', tabletRouter);
+    app.use('/accessories', accessoryRouter);
+    app.use('/phones', phoneRouter);
+    app.use('/users', userRouter);
+    app.use("/orders", orderRouter);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error: Error) => {
+    console.error('Unable to connect to the database:', error);
+  });
