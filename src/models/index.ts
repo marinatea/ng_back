90032e25b0d6d1c43d445740b6873a5d38dc5df4
Maryna Karpova <mari.artarea@gmail.com>
2024-09-16 15:34:31 +0200

    config

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,24 +1,21 @@
-import { AccessoryMap } from './accessory';
-import { PhoneMap } from './phone';
-import { ProductMap } from './product';
-import { Sequelize } from 'sequelize-typescript';
-import { TabletMap } from './tablet';
-import { UserMap } from './user';
-import dotenv from 'dotenv';
+import { AccessoryMap } from "./accessory";
+import { PhoneMap } from "./phone";
+import { ProductMap } from "./product";
+import { Sequelize } from "sequelize-typescript";
+import { TabletMap } from "./tablet";
+import { UserMap } from "./user";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined in environment variables');
+  throw new Error("DATABASE_URL is not defined in environment variables");
 }
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: "postgres",
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    ssl: false,
   },
   logging: false,
 });
